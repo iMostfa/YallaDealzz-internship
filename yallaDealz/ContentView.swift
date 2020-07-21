d6be871166862948806ebdb9e6f9b242afec97b0
Mostfa <almme111@gmail.com>
2020-07-21 14:58:58 +0200

    Initial Commit

---
+++ yallaDealz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yallaDealz
+//
+//  Created by mostfa on 7/21/20.
+//  Copyright © 2020 mostfaE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
